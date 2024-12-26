27d7aa67d282213a79fe8bbc26ddb3596bca6c18
AsteroidDestroyer123 <Fedytrojan@yandex.com>
2024-12-26 15:58:17 +0300

    Update y3z6.py

--- lab5/y3z6.py
+++ lab5/y3z6.py
@@ -7,6 +7,7 @@ a=np.array([[1,23,51,8],
             [23,12,6,9]
             ])
 def f(a):
+    
     size=a.shape[0]
     mx1=a[0][0]
     j1=0
@@ -14,17 +15,24 @@ def f(a):
         if a[i][i]>mx1:
             j1=i
             mx1=a[i][i]
+    
     mx2=a[0][0]
     j2=0
     for i in range(size):
-        if a[0][i]>mx2:
+        if a[0][i]>=mx2:
             j2=i
             mx2=a[0][i]
+    if j1==j2:
+        for i in range(size):
+            if i!=j1:
+                if a[0][i]>=mx2:
+                    j2=i
+                    mx2=a[0][i]
     for i in range(size):
         a[i][j1], a[i][j2]= a[i][j2], a[i][j1]
     return a
 #пример
-a=np.array([[1,23,51,8],
+a=np.array([[51,23,51,8],
             [23,14,2,6],
             [23,1,5,23],
             [23,12,6,9]
