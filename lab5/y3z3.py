bd48b1e5ea3bb67b45383af306e7c3da31ede1f1
AsteroidDestroyer123 <Fedytrojan@yandex.com>
2024-12-26 15:44:14 +0300

    Update y3z3.py

--- lab5/y3z3.py
+++ lab5/y3z3.py
@@ -1,21 +1,42 @@
 #вычислить сумму эдементов с нечетным индексом, предварительно поменяв попарно элемнты начиная с первого, если первой элемнт больше среднего арифметического всей прогресии 
 # и с последнего элемента в противном случае
-def f(m):
+def perestanovka(p):
     c=0
-    for i in m:
+    x=p.copy()
+    for i in x:
         c+=i
-    c=c/len(m)
-    if m[0]>c:
-        for i in range(0,len(m)-1,2):
-            m[i],m[i+1]=m[i+1],m[i]
+    srzn=c/len(x)
+    if len(x)%2==0:
+            for i in range(0,len(x)-1,2):
+                t=x[i]
+                p=x[i+1]
+                x[i]=p
+                x[i+1]=t
     else:
-        for i in range(len(m)-1,0,-2):
-            m[i],m[i-1]=m[i-1],m[i]
-    p=0
-    for i in range(len(m)):
-        if i %2==1:
-            p+=i
-    return p
+        if x[0]>srzn:
+            for i in range(0,len(x)-2,2):
+                t=x[i]
+                p=x[i+1]
+                x[i]=p
+                x[i+1]=t
+        else:
+            for i in range(len(x)-1,2,-2):
+                t=x[i]
+                p=x[i-1]
+                x[i]=p
+                x[i-1]=t
+    return x
+
+def f(m):
+    t=perestanovka(m)
+    k=0
+    for i in range(len(t)):
+        if i%2==1:
+            k+=t[i]
+    return k
+
+
 #пример 
 m=[6,1,3,9,4,6,8,5,3,1,23,9,6,0,0]
+
 print(f(m))
