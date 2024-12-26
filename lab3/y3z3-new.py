4f8275ea9609296882d4760ea1872b43df4f323b
AsteroidDestroyer123 <Fedytrojan@yandex.com>
2024-12-26 11:58:36 +0300

    Update y3z3-new.py

--- lab3/y3z3-new.py
+++ lab3/y3z3-new.py
@@ -1,16 +1,13 @@
 #Поменять местами сосдение элементы перед максимальным(1 с 2, 3 с 4)
-m=input('вводите числа, которые войдут в массив, через пробел').split(' ')
+m=input('вводите числа, которые войдут в массив, через пробел: ').split(' ')
 k=[]
 for i in m:
     k.append(int(i))
 
 
 c=k.index(max(k))
-if c%2==0:
-    for i in range(0,c-1,2):
-        k[i],k[i+1]=k[i+1],k[i]
-else:
-    for i in range(0,c-2,2):
-        k[i],k[i+1]=k[i+1],k[i]
+
+for i in range(0,c,2):
+    k[i],k[i+1]=k[i+1],k[i]
 
 print(k)
