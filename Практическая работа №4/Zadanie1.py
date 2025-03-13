52a9c5234689b0a05f3c805d69e50484516a3ea1
AsteroidDestroyer123 <Fedytrojan@yandex.com>
2025-03-13 23:39:11 +0300

    Update Zadanie1.py

--- Практическая работа №4/Zadanie1.py
+++ Практическая работа №4/Zadanie1.py
@@ -1,16 +1,16 @@
-#a+bx^2+c*arctan(x)
+#Напишите программу, строящую график функции. Коэффициенты a,b,c и диапазон задаются с клавиатуры.
+# a+bx^2+c*arctan(x)
 import matplotlib.pyplot as plt
 import numpy as np
 
-a=2#коэффицент а
-b=1# -//- b
+a=12#коэффицент а
+b=0.3# -//- b
 c=3#-//- c
 rang1=-10#начало диапазона
 rang2=10#конец диапазона
 
-x=np.linspace(rang1,rang2,abs(rang2-rang1)*10)
-print(x)
-y=a+b*x**2+c*np.arctan(x)
+x=np.linspace(rang1,rang2,abs(rang2-rang1)*10)#массив с значениями х
+y=a+b*x**2+c*np.arctan(x)#массив с значениями функции
 fig,ax=plt.subplots()
 ax.plot(x,y)
 plt.show()
