3646a1f97c1c8be39978a053a614dbc32e8013fd
AsteroidDestroyer123 <Fedytrojan@yandex.com>
2025-03-13 23:25:12 +0300

    Update z15.py

--- lab9/z15.py
+++ lab9/z15.py
@@ -1,17 +1,11 @@
 #15. Заданы массивы А и В, содержащие n и m элементов соответственно. 
 #Вставить массив В между k-м и (k + 1)-м элементами массива А (k задано).
-with open("massivA.txt", "w", encoding="utf-8") as file_a:
-    file_a.write("whieefwhiefwhiohiwefihewfoiddjkjsd")
-with open("massivB.txt", "w", encoding="utf-8") as file_b:
-    file_b.write("MEFMWKNSDKMSDKMMSAFWO;EOKSADFLK;")
 with open("massivA.txt", "r") as file_a:
     a = file_a.read() 
 with open("massivB.txt", "r") as file_a:
     b = file_a.read() 
 k=5
-with open("massivA.txt","w") as f:
-    f.write(a[:k]+b+a[k:])
-with open("massivA.txt",'r') as f:
-    result=f.read()
+result = b[:k]+a+b[k:]
 print(result)
-
+with open("massivB.txt", "w") as f:
+    f.write(result)
