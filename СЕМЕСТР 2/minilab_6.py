d0a716dfc7f7f1b8d18eb51e8efb51cc51b8ef2f
AsteroidDestroyer123 <Fedytrojan@yandex.com>
2025-05-26 13:36:59 +0300

    Update minilab_6.py

--- СЕМЕСТР 2/minilab_6.py
+++ СЕМЕСТР 2/minilab_6.py
@@ -1,9 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-def gradspusk(func=lambda x: np.exp(-x**2),
-                    diffFunc=lambda x: -2*x*np.exp(-x**2),
+#y=exp(x**2)
+def gradspusk(func=lambda x: np.exp(x**2),
+                    diffFunc=lambda x: 2*x*np.exp(x**2),
                     x0=3,
                     speed=0.1,
                     epochs=1000):
@@ -12,27 +12,27 @@ def gradspusk(func=lambda x: np.exp(-x**2),
     y_list = [func(x)]
     
     for i in range(epochs):
-        x = x + speed * diffFunc(x)
+        x = x - speed * diffFunc(x)
         x_list.append(x)
         y_list.append(func(x))
     
     return x_list, y_list
 
 
-func = lambda x: np.exp(-x**2)
-diffFunc = lambda x: -2*x*np.exp(-x**2)
+func = lambda x: np.exp(x**2)
+diffFunc = lambda x: 2*x*np.exp(x**2)
 
 
-x0 = 3
-speed = 1
-epochs = 500
+x0 = 1
+speed = 0.1
+epochs = 1000
 x_list, y_list = gradspusk(func, diffFunc, x0, speed, epochs)
 
 
-x = np.linspace(-5, 5, 400)
+x = np.linspace(-1, 1, 1000)
 y = func(x)
 
-plt.figure(figsize=(10, 6))
+
 plt.plot(x, y, label='f(x)', color='blue')
 plt.scatter(x_list, y_list, color='red',s=20)
 plt.scatter(x_list[-1],y_list[-1],marker='*',s=200)
@@ -40,4 +40,3 @@ plt.legend()
 plt.grid(True)
 plt.show()
 
-#Граничное значение параметра speed равно 1
